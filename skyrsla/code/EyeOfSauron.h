70e5873080faeebe4ffb6b362ed39609f9dacbcf
SigursteinnOli <34093550+SigursteinnOli@users.noreply.github.com>
2019-12-05 14:03:13 +0000

    Update EyeOfSauron.h

--- skyrsla/code/EyeOfSauron.h
+++ skyrsla/code/EyeOfSauron.h
@@ -1,1 +1,12 @@
 
+/*vex-vision-config:begin*/
+#include "vex.h"
+vex::vision::signature RED_TARGET = vex::vision::signature (1, 5503, 6079, 5790, -383, -159, -270, 5.1, 0);
+vex::vision::signature SIG_2 = vex::vision::signature (2, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision::signature SIG_3 = vex::vision::signature (3, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision::signature SIG_4 = vex::vision::signature (4, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision::signature SIG_5 = vex::vision::signature (5, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision::signature SIG_6 = vex::vision::signature (6, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision::signature SIG_7 = vex::vision::signature (7, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision Vision12 = vex::vision (vex::PORT12, 50, RED_TARGET, SIG_2, SIG_3, SIG_4, SIG_5, SIG_6, SIG_7);
+/*vex-vision-config:end*/
